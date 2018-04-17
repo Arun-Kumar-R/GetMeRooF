988623a61c9f91ef41e11db4b71924a2f36b5866
Arun-Kumar-R <rarunkumar0298@gmail.com>
2018-04-17 22:23:09 +0530

    npm start

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-   database: 'mongodb://localhost:27017/RoofDB',
+   //database: 'mongodb://localhost:27017/RoofDB',
+   database:'mongodb://Arun:austino@ds117956.mlab.com:17956/roofdb',
    secret: 'yoursecret'
 }
