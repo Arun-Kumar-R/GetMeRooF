a4075a3f557e04a411e3b093b891bc2a16cf83ee
Arun kumar R <36698089+Arun-Kumar-R@users.noreply.github.com>
2018-08-01 17:48:51 +0530

    db set locally

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-   //database: 'mongodb://localhost:27017/RoofDB',
-   database:'mongodb://Arun:austino@ds117956.mlab.com:17956/roofdb',
+   database: 'mongodb://localhost:27017/RoofDB',
    secret: 'yoursecret'
 }
